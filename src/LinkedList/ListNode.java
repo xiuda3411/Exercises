f0f753bd50260c985c88c82285b7bd8bb2983907
xiuda3411 <1157867526@qq.com>
2020-10-02 22:17:54 +0800

    链表:bicyclist:

---
+++ src/LinkedList/ListNode.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+/**
+ * @program: Exercises
+ * @description: 两数相加
+ * @author: 宋丽
+ * @create: 2020-10-02 19:34
+ **/
+
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+}
