3be54ecd98bf9aa028efa5112a098abd93e5bdc8
xiuda3411 <1157867526@qq.com>
2020-10-02 22:19:31 +0800

    两数相加:two_women_holding_hands:

--- src/LinkedList/AddTwoNumbers.java
+++ src/LinkedList/AddTwoNumbers.java
@@ -1,6 +1,18 @@
 package LinkedList;
 
+/**
+ * @program: Exercises
+ * @description: 两数相加
+ * @author: 宋丽
+ * @create: 2020-10-2 20:12
+ **/
 public class AddTwoNumbers {
+    /**
+     * 中规中矩
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1,q = l2,current =  dummyHead;
