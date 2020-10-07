f4fead616e701e07930de29af69d27ea01cde440
xiuda3411 <1157867526@qq.com>
2020-10-07 21:27:23 +0800

    二叉树结点

---
+++ src/LinkedList/TreeNode.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+/**
+ * @program: Exercises
+ * @description: 二叉树结点
+ * @author: 宋丽
+ * @create: 2020-10-07 20:15
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
