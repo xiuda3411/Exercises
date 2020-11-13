10e6630fd32fe0dd82e028a846ef522bf4e10fd5
xiuda3411 <1157867526@qq.com>
2020-11-13 19:52:50 +0800

    奇偶链表

--- src/LinkedList/OddEvenList.java
+++ src/LinkedList/OddEvenList.java
@@ -34,4 +34,29 @@ public class OddEvenList {
         }
         return head;
     }
+
+    /**
+     *
+     * @param head
+     * @return
+     */
+    public ListNode oddEvenList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // head 为奇链表头结点，o 为奇链表尾节点
+        ListNode o = head;
+        // p 为偶链表头结点
+        ListNode p = head.next;
+        // e 为偶链表尾节点
+        ListNode e = p;
+        while (o.next != null && e.next != null) {
+            o.next = e.next;
+            o = o.next;
+            e.next = o.next;
+            e = e.next;
+        }
+        o.next = p;
+        return head;
+    }
 }
