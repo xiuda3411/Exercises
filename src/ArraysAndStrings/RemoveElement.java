520f12bc52a017747bdd5446e55d770588177b98
xiuda3411 <1157867526@qq.com>
2020-11-19 18:32:32 +0800

    更新更简便方法

--- src/ArraysAndStrings/RemoveElement.java
+++ src/ArraysAndStrings/RemoveElement.java
@@ -29,4 +29,21 @@ public class RemoveElement {
         }
         return start;
     }
+
+    /**
+     * 双指针
+     * 齐头并进
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement2(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
 }
