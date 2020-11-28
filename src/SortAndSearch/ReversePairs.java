ed776450b37b40af3af67d7d4dfd2b9b3f2132bf
xiuda3411 <1157867526@qq.com>
2020-11-28 18:09:39 +0800

    翻转对

---
+++ src/SortAndSearch/ReversePairs.java
@@ -0,0 +1,129 @@
+package SortAndSearch;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @program: Exercises
+ * @description: 翻转对
+ * @author: 宋丽
+ * @create: 2020-11-28 18:07
+ **/
+public class ReversePairs {
+    /**
+     * 归并排序
+     * @param nums
+     * @return
+     */
+    public int reversePairs(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        return reversePairsRecursive(nums, 0, nums.length - 1);
+    }
+
+    public int reversePairsRecursive(int[] nums, int left, int right) {
+        if (left == right) {
+            return 0;
+        } else {
+            int mid = (left + right) / 2;
+            int n1 = reversePairsRecursive(nums, left, mid);
+            int n2 = reversePairsRecursive(nums, mid + 1, right);
+            int ret = n1 + n2;
+
+            // 首先统计下标对的数量
+            int i = left;
+            int j = mid + 1;
+            while (i <= mid) {
+                while (j <= right && (long) nums[i] > 2 * (long) nums[j]) {
+                    j++;
+                }
+                ret += j - mid - 1;
+                i++;
+            }
+
+            // 随后合并两个排序数组
+            int[] sorted = new int[right - left + 1];
+            int p1 = left, p2 = mid + 1;
+            int p = 0;
+            while (p1 <= mid || p2 <= right) {
+                if (p1 > mid) {
+                    sorted[p++] = nums[p2++];
+                } else if (p2 > right) {
+                    sorted[p++] = nums[p1++];
+                } else {
+                    if (nums[p1] < nums[p2]) {
+                        sorted[p++] = nums[p1++];
+                    } else {
+                        sorted[p++] = nums[p2++];
+                    }
+                }
+            }
+            for (int k = 0; k < sorted.length; k++) {
+                nums[left + k] = sorted[k];
+            }
+            return ret;
+        }
+    }
+
+    /**
+     * 树状数组
+     * @param nums
+     * @return
+     */
+    public int reversePairs2(int[] nums) {
+        Set<Long> allNumbers = new TreeSet<Long>();
+        for (int x : nums) {
+            allNumbers.add((long) x);
+            allNumbers.add((long) x * 2);
+        }
+        // 利用哈希表进行离散化
+        Map<Long, Integer> values = new HashMap<Long, Integer>();
+        int idx = 0;
+        for (long x : allNumbers) {
+            values.put(x, idx);
+            idx++;
+        }
+
+        int ret = 0;
+        BIT bit = new BIT(values.size());
+        for (int i = 0; i < nums.length; i++) {
+            int left = values.get((long) nums[i] * 2), right = values.size() - 1;
+            ret += bit.query(right + 1) - bit.query(left + 1);
+            bit.update(values.get((long) nums[i]) + 1, 1);
+        }
+        return ret;
+    }
+}
+
+class BIT {
+    int[] tree;
+    int n;
+
+    public BIT(int n) {
+        this.n = n;
+        this.tree = new int[n + 1];
+    }
+
+    public static int lowbit(int x) {
+        return x & (-x);
+    }
+
+    public void update(int x, int d) {
+        while (x <= n) {
+            tree[x] += d;
+            x += lowbit(x);
+        }
+    }
+
+    public int query(int x) {
+        int ans = 0;
+        while (x != 0) {
+            ans += tree[x];
+            x -= lowbit(x);
+        }
+        return ans;
+    }
+}
